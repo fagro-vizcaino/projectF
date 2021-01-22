f779c97af54603a9ad911345677987a2c520fe98
Fagro <fagro.vizcaino@outlook.com>
2021-01-21 21:33:17 -0400

    added purchase order entity classes

---
+++ ProjectF.Data/Entities/PurchaseOrders/PurchaseOrderDetail.cs
@@ -0,0 +1,77 @@
+﻿using ProjectF.Data.Entities.Common;
+using ProjectF.Data.Entities.Common.ValueObjects;
+
+namespace ProjectF.Data.Entities.PurchaseOrders
+{
+    public class PurchaseOrderDetail : _BaseEntity
+    {
+        public Code ProductCode { get; private set; }
+        public Name ProductName { get; private set; }
+        public Name Warehouse { get; private set; }
+        public int WarehouseId { get; private set; }
+        public decimal Qty { get; private set; }
+        public decimal Cost { get; private set; }
+        public decimal TaxPercent { get; private set; }
+        public PurchaseOrderHeader PurchaseOrderHeader { get; private set; }
+
+        public PurchaseOrderDetail() { }
+
+        public PurchaseOrderDetail(Code productCode
+            , Name productName
+            , Name warehouse
+            , int warehouseId
+            , decimal qty
+            , decimal cost
+            , decimal taxpercent
+            , PurchaseOrderHeader purchaseOrderHeader)
+        {
+            ProductCode         = productCode;
+            ProductName         = productName;
+            Warehouse           = warehouse;
+            WarehouseId         = warehouseId;
+            Qty                 = qty;
+            Cost                = cost;
+            TaxPercent          = taxpercent;
+            PurchaseOrderHeader = purchaseOrderHeader;
+        }
+
+        public void Deconstruct(out Code dproductCode
+            , out Name dproductName
+            , out Name dwarehouse
+            , out int dwarehouseId
+            , out decimal dqty
+            , out decimal dcost
+            , out decimal dtaxpercent
+            , out PurchaseOrderHeader dpurchaseOrderHeader)
+        {
+            dproductCode         = ProductCode;
+            dproductName         = ProductName;
+            dwarehouse           = Warehouse;
+            dwarehouseId         = WarehouseId;
+            dqty                 = Qty;
+            dcost                = Cost;
+            dtaxpercent          = TaxPercent;
+            dpurchaseOrderHeader = PurchaseOrderHeader;
+        }
+
+
+        public void EditPurchaseOrderDetail(Code productCode
+            , Name productName
+            , Name warehouse
+            , int warehouseId
+            , decimal qty
+            , decimal cost
+            , decimal taxpercent
+            , PurchaseOrderHeader purchaseOrderHeader)
+        {
+            ProductCode         = productCode;
+            ProductName         = productName;
+            Warehouse           = warehouse;
+            WarehouseId         = warehouseId;
+            Qty                 = qty;
+            Cost                = cost;
+            TaxPercent          = taxpercent;
+            PurchaseOrderHeader = purchaseOrderHeader;
+        }
+    }
+}
---
+++ ProjectF.Data/Entities/PurchaseOrders/PurchaseOrderHeader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectF.Data.Entities.Common;
+using ProjectF.Data.Entities.Common.ValueObjects;
+using ProjectF.Data.Entities.PaymentList;
+using ProjectF.Data.Entities.Suppliers;
+using ProjectF.Data.Entities.Taxes;
+
+namespace ProjectF.Data.Entities.PurchaseOrders
+{
+    public class PurchaseOrderHeader : _BaseEntity
+    {
+        public Code Code { get; private set; }
+        public Name SupplierName { get; private set; }
+        public string Rnc { get; private set; }
+        public GeneralText Address { get; private set; }
+        public DateTime DeliverDate { get; private set; }
+        public PaymentTerm PaymentTerm { get; private set; }
+        public GeneralText Notes { get; private set; }
+
+        private readonly List<PurchaseOrderDetail> _purchaseOrderDetails = new List<PurchaseOrderDetail>();
+        public  IReadOnlyList<PurchaseOrderDetail> PurchaseDetails => _purchaseOrderDetails.ToList();
+        public PurchaseOrderHeader() { }
+
+        public PurchaseOrderHeader(Code code
+            , Name supplierName
+            , string rnc
+            , GeneralText address
+            , DateTime deliverDate
+            , PaymentTerm paymentTerm
+            , Tax tax
+            , GeneralText note
+            , List<PurchaseOrderDetail> purchaseOrderDetails
+            , DateTime created
+            , EntityStatus status = EntityStatus.Active)
+        {
+            Code            = code;
+            SupplierName    = supplierName;
+            Rnc             = rnc;
+            Address         = address;
+            DeliverDate     = deliverDate;
+            PaymentTerm     = paymentTerm;
+            Notes           = note;
+            Status          = status;
+            Created         = created == DateTime.MinValue ? DateTime.UtcNow : created;
+            _purchaseOrderDetails.AddRange(CreateDetails(purchaseOrderDetails));
+        }
+
+
+        IEnumerable<PurchaseOrderDetail> CreateDetails(IEnumerable<PurchaseOrderDetail> purchaseOrderDetails)
+        {
+            var purchaseOrderHeader = this;
+            return purchaseOrderDetails.Map(d => new PurchaseOrderDetail());
+        }
+
+        public void EditPurchaseOrderHeader(Code code
+            , Name supplierName
+            , string rnc
+            , GeneralText address
+            , DateTime deliverDate
+            , PaymentTerm paymentTerm
+            , Tax tax
+            , GeneralText note
+            , List<PurchaseOrderDetail> purchaseOrderDetails
+            , EntityStatus status = EntityStatus.Active)
+        {
+            var header = this;
+            var _localpurchaseOrder = purchaseOrderDetails.Map(d =>
+                new PurchaseOrderDetail());
+
+            Code            = code;
+            SupplierName    = supplierName;
+            Rnc             = rnc;
+            Address         = address;
+            DeliverDate     = deliverDate;
+            PaymentTerm     = paymentTerm;
+            Notes           = note;
+            Status          = status;
+            Modified        = DateTime.UtcNow;
+            _purchaseOrderDetails.AddRange(_localpurchaseOrder);
+        }
+    }
+}
