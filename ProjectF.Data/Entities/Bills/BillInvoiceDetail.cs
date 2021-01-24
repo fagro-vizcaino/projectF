59ec64732c29b58dd39a2f2dc8de205c4756db2c
Fagro <fagro.vizcaino@outlook.com>
2021-01-24 13:13:59 -0400

    added bill invoice and goods type repository

--- ProjectF.Data/Entities/Bills/BillInvoiceDetail.cs
+++ ProjectF.Data/Entities/Bills/BillInvoiceDetail.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectF.Data.Entities.Common;
+using ProjectF.Data.Entities.Common.ValueObjects;
 
 namespace ProjectF.Data.Entities.Bills
 {
-    public class BillInvoiceDetail
+    public class BillInvoiceDetail : _BaseEntity
     {
+        public Code ProductCode { get; private set; }
+        public Name ProductName { get; private set; }
+        public decimal Amount { get; private set; }
+        public decimal Qty { get; private set; }
+        public decimal DiscountValue { get; private set; }
+        public decimal TaxPercent { get; private set; }
+        public BillInvoiceHeader BillInvoiceHeader { get; private set; }
+
+        protected BillInvoiceDetail() { }
+
+        public BillInvoiceDetail(Code productCode
+            , Name productName
+            , decimal amount
+            , decimal qty
+            , decimal discountValue
+            , decimal taxPercent
+            , BillInvoiceHeader billInvoiceHeader)
+        {
+            ProductCode       = productCode;
+            ProductName       = productName;
+            Amount            = amount;
+            Qty               = qty;
+            DiscountValue     = discountValue;
+            TaxPercent        = taxPercent;
+            BillInvoiceHeader = billInvoiceHeader;
+        }
+
+        public void Deconstruct(out Code dproductCode
+            , out Name dproductName
+            , out decimal dqty
+            , out decimal damount
+            , out decimal dtaxpercent
+            , out decimal ddiscountValue
+            , out BillInvoiceHeader dbillInvoiceHeader)
+        {
+            dproductCode       = ProductCode;
+            dproductName       = ProductName;
+            ddiscountValue     = DiscountValue;
+            dqty               = Qty;
+            damount            = Amount;
+            dtaxpercent        = TaxPercent;
+            dbillInvoiceHeader = BillInvoiceHeader;
+        }
     }
 }
--- ProjectF.Data/Entities/Bills/BillInvoiceHeader.cs
+++ ProjectF.Data/Entities/Bills/BillInvoiceHeader.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ProjectF.Data.Entities.Common;
 using ProjectF.Data.Entities.Common.ValueObjects;
 
@@ -13,13 +11,140 @@ namespace ProjectF.Data.Entities.Bills
         public Code Code { get; private set; }
         public Name SupplierName { get; private set; }
         public int SupplierId { get; private set; }
+        public DateTime BillDate { get; private set; }
+        public DateTime BillDueDate { get; private set; }
+        public Code InvoiceNumber { get; private set; }
+        public Code Ncf { get; private set; }
+        public string Rnc { get; private set; }
+        public string PaymentTerm { get; private set; }
+        public int PaymentTermId { get; private set; }
+        public string WarehouseName { get; private set; }
+        public int WarehouseId { get; private set; }
+        public string GoodsTypeName { get; private set; }
+        public int GoodsTypeId { get; private set; }
+        public GeneralText Notes { get; private set; }
+        public decimal Subtotal { get; private set; }
+        public decimal TaxTotal { get; private set; }
+        public decimal DiscountTotal { get; private set; }
+        public decimal Total { get; private set; }
+        
+        private readonly List<BillInvoiceDetail> _billInvoiceDetails = new();
+        public IReadOnlyList<BillInvoiceDetail> BillInvoiceDetails => _billInvoiceDetails.ToList();
+        protected BillInvoiceHeader() { }
 
-        public DateTime BillDate { get; set; }
-        public Code InvoiceNumber { get; set; }
-        public Code Ncf { get; set; }
-        public string PaymentTerm { get; set; }
-        public int PaymentTermId { get; set; }
-        public int MyProperty { get; set; }
+        public BillInvoiceHeader(Code code
+            , Name supplierName
+            , int supplierId
+            , DateTime billDate
+            , DateTime billDueDate
+            , Code invoiceNumber
+            , Code ncf
+            , string rnc
+            , string paymentTerm
+            , int paymentTermId
+            , string warehouseName
+            , int warehouseId
+            , string goodsTypeName
+            , int goodsTypeId
+            , GeneralText notes
+            , decimal subTotal
+            , decimal taxTotal
+            , decimal discountTotal
+            , decimal total
+            , List<BillInvoiceDetail> billInvoiceDetails
+            , DateTime created
+            , EntityStatus status = EntityStatus.Active)
+        {
+            Code               = code;
+            SupplierName       = supplierName;
+            SupplierId         = supplierId;
+            BillDate           = billDate;
+            BillDueDate        = billDueDate;
+            InvoiceNumber      = invoiceNumber;
+            Ncf                = ncf;
+            Rnc                = rnc;
+            PaymentTerm        = paymentTerm;
+            PaymentTermId      = paymentTermId;
+            WarehouseName      = warehouseName;
+            WarehouseId        = warehouseId;
+            GoodsTypeName      = goodsTypeName;
+            GoodsTypeId        = goodsTypeId;
+            Notes              = notes;
+            Subtotal           = subTotal;
+            TaxTotal           = taxTotal;
+            DiscountTotal      = discountTotal;
+            Total              = total;
+            Created            = created == DateTime.MinValue ? DateTime.UtcNow : created;
+            Status             = status;
 
+            _billInvoiceDetails.AddRange(CreateDetails(billInvoiceDetails));
+        }
+
+        private IEnumerable<BillInvoiceDetail> CreateDetails(IEnumerable<BillInvoiceDetail> billInvoiceDetails)
+        {
+            var billInvoiceHeader = this;
+            return billInvoiceDetails.Map(d => new BillInvoiceDetail(d.ProductCode
+                , d.ProductName
+                , d.Amount
+                , d.Qty
+                , d.DiscountValue
+                , d.TaxPercent
+                , billInvoiceHeader));
+        }
+
+        public void EditBillInvoiceHeader(Code code
+            , Name supplierName
+            , int supplierId
+            , DateTime billDate
+            , DateTime billDueDate
+            , Code invoiceNumber
+            , Code ncf
+            , string rnc
+            , string paymentTerm
+            , int paymentTermId
+            , string warehouseName
+            , int warehouseId
+            , string goodsTypeName
+            , int goodsTypeId
+            , GeneralText notes
+            , decimal subTotal
+            , decimal taxTotal
+            , decimal discountTotal
+            , decimal total
+            , List<BillInvoiceDetail> billInvoiceDetails)
+        {
+            var header = this;
+            var _localBillInvoiceDetals = billInvoiceDetails.Map(d =>
+                new BillInvoiceDetail(d.ProductCode
+                , d.ProductName
+                , d.Amount
+                , d.Qty
+                , d.DiscountValue
+                , d.TaxPercent
+                , header));
+
+            Code          = code;
+            SupplierName  = supplierName;
+            SupplierId    = supplierId;
+            Rnc           = rnc;
+            Ncf           = ncf;
+            InvoiceNumber = invoiceNumber;
+            BillDate      = billDate;
+            BillDueDate   = billDueDate;
+            PaymentTerm   = paymentTerm;
+            PaymentTermId = paymentTermId;
+            WarehouseName = warehouseName;
+            WarehouseId   = warehouseId;
+            GoodsTypeName = goodsTypeName;
+            GoodsTypeId   = goodsTypeId;
+            Notes         = notes;
+            Subtotal      = subTotal;
+            TaxTotal      = taxTotal;
+            DiscountTotal = discountTotal;
+            Total         = total;
+            Status        = EntityStatus.Active;
+            Modified      = DateTime.UtcNow;
+            _billInvoiceDetails.AddRange(_localBillInvoiceDetals);
+        }
     }
 }
